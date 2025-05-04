de5413ef97df34ba8b7b2c2c5f4a6ff823d4e0c8
hasibulkabiremon <hasibul15-10673@diu.edu.bd>
2025-05-04 14:51:49 +0600

    create a database

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy #ORM(object relational mapper)
 
 app = Flask(__name__)
 
+#create database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///travel.db"
+
+db = SQLAlchemy(app)
+
+class Destination(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    destination = db.Column(db.String(50),nullable=False)
+    country = db.Column(db.String(50),nullable=False)
+    rating = db.Column(db.Float,nullable=False)
+
+    def to_dict(self):
+        return{
+            "id":self.id,
+            "destination":self.destination,
+            "country":self.country,
+            "rating":self.rating
+        }
+
+with app.app_context():
+    db.create_all()
 
 
 #create Routes
